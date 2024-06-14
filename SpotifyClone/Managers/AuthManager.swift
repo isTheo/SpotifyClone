d96e609a91d0dfac36b9995882ab9afc7b1430be
isTheo <matteohg07@gmail.com>
2024-06-14 21:00:04 +0200

    a lot of updates

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -5,11 +5,18 @@
 //  Created by Matteo Orru on 14/06/24.
 //
 
+// Handling all the authentication related logic.
+
 import Foundation
 
 
 final class AuthManager {
     static let shared = AuthManager()
+    
+    struct Constants {
+        static let clientID = "e7696031f77a4e6e8d502a6cd073f832"
+        static let clientSecret = "9b5b8696fa1445a8b3f993098d199cc7"
+    }
     
     private init() {}
     
@@ -17,6 +24,7 @@ final class AuthManager {
         return false
     }
     
+// Spotify uses the OAuth 2.0 standard which has principles of getting an access token on behalf of the user. Since that token expires after a certain number of times we need to get a refresh token.
     private var accessToken: String? {
         return nil
     }
