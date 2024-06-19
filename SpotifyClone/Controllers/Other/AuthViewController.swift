a0406d0a3864037e2551f38b4e0d6c618abecb6b
isTheo <matteohg07@gmail.com>
2024-06-19 15:01:21 +0200

    project updated

--- SpotifyClone/Controllers/Other/AuthViewController.swift
+++ SpotifyClone/Controllers/Other/AuthViewController.swift
@@ -34,6 +34,12 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         view.backgroundColor = .systemBackground
         webView.navigationDelegate = self
         view.addSubview(webView)
+        
+        guard let url = AuthManager.shared.signInURL else {
+            return
+        }
+        //this line shows up the login page
+        webView.load(URLRequest(url: url))
     }
     
     
@@ -42,4 +48,30 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         
         webView.frame = view.bounds
     }
+    
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        guard let url = webView.url else {
+            return
+        }
+        //exchange the code for the access token
+        guard let code = URLComponents(string: url.absoluteString)?.queryItems?.first(where: { $0.name == "code" })?.value else {
+            return
+        }
+        webView.isHidden = true
+        
+        print("Code: \(code)")
+        
+        AuthManager.shared.exchangeCodeForToken(code: code) { [weak self] success in
+            DispatchQueue.main.async {
+                self?.navigationController?.popViewController(animated: true)
+                self?.completionHandler?(success)
+            }
+
+        }
+    }
+    
+    
+    
+    
 }
--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -24,7 +24,7 @@ final class AuthManager {
         let scopes = "user-read-private"
         let redirectURI = "https://ormatteo.wordpress.com"
         let base = "https://accounts.spotify.com/authorize"
-        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
         
         return URL(string: string)
         
@@ -50,6 +50,31 @@ final class AuthManager {
     private var shouldRefreshToken: Bool {
         return false
     }
+    
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    
+    
+    ) {
+        //get token
+    }
+    
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
+    }
+    
+    
+    
+    
+    
+    
     
     
     
