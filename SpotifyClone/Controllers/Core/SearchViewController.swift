53607cb3329633a79830032a1226e8c913d69b2c
isTheo <matteohg07@gmail.com>
2024-07-20 23:52:55 +0200

    project updated

--- SpotifyClone/Controllers/Core/SearchViewController.swift
+++ SpotifyClone/Controllers/Core/SearchViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+    
+    let searchController: UISearchController = {
+        let results = UIViewController()
+        results.view.backgroundColor = .red
+        let vc = UISearchController(searchResultsController: results)
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        
+        
+        return vc
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
     }
+    
+    
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        print(query)
+    }
+    
+    // Perform search
     
     
     
