450f68bb95a4f2497a065d8809baac114705e414
isTheo <matteohg07@gmail.com>
2024-09-03 19:25:45 +0200

    project updated

--- SpotifyClone/Controllers/Other/Library/LibraryPlaylistsViewController.swift
+++ SpotifyClone/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -186,6 +186,7 @@ extension LibraryPlaylistsViewController: UITableViewDelegate, UITableViewDataSo
         
         let vc = PlaylistViewController(playlist: playlist)
         vc.navigationItem.largeTitleDisplayMode = .never
+        vc.isOwner = true
         navigationController?.pushViewController(vc, animated: true)
     }
     
